0d4d9af6a201b23d238c92e021138e36913f04b3
SowjiKrish <120230840+SowjiKrish@users.noreply.github.com>
2022-12-10 07:42:07 +0530

    Add files via upload
    Addition of two numbers

---
+++ sumoftwonumbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  printf("enter any number= ");
+  scanf ("%d",&a);
+  printf("enter second number= ");
+  scanf("%d",&b);
+  printf("sum of two numbers=");
+  int c=a+b;
+  printf("%d",c);
+  return 0;
+
+
+
+}
