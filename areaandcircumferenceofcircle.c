ca67139c8de8680bc4b425260d287c1be9294e51
SowjiKrish <120230840+SowjiKrish@users.noreply.github.com>
2022-12-11 11:09:28 +0530

    Add files via upload

---
+++ areaandcircumferenceofcircle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int r;
+  float a =3.14;
+  printf ("enter the value of r=\n");
+  scanf ("%d",&r);
+  float s=a*r*r;
+  printf ("area of circle= \n");
+  printf ("%f",s);
+  float j=2*3.14*r;
+  printf ("circumference of the circle=");
+  printf ("%f",j);
+  return 0;}
