d7063531735370b4c45921afc283eee09646c907
SowjiKrish <120230840+SowjiKrish@users.noreply.github.com>
2022-12-14 21:35:40 +0530

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int s;
+  printf("enter the value of s");
+  scanf ("%d",&s);
+  if (s<20)
+  {
+    printf("s is less than 20");
+  }
+  else
+  {
+    printf("s is greater than 20");
+  }
+}
---
+++ minutes.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,s,j,c;
+  s =60;
+  j= 60;
+  printf ("enter any value");
+  scanf ("%d",&a);
+  b=a*s;
+  c=a*j;
+  printf("number of minutes = ");
+  printf("%d",b);
+  printf("number of seconds= ");
+  printf ("%d",&c);
+  return 0;
+}
