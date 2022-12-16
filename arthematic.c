ca8649a04344298afd54168b1fd5726667bc2ad2
SowjiKrish <120230840+SowjiKrish@users.noreply.github.com>
2022-12-16 21:09:39 +0530

    Add files via upload

---
+++ arthematic.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  printf("enter the values of a=\n");
+  scanf("%d",&a);
+  printf("enter the value of b=\n");
+  scanf("%d",&b);
+  printf("addition=%d\n",(a+b));
+  printf("subtraction=%d\n",(a-b));
+  printf("multiplication=%d\n",(a*b));
+  printf("division=%d\n",(a/b));
+  printf("modulus division=%d\n",(a%b));
+  return 0;
+
+
+
+
+}
