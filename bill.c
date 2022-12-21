66c1751687aab7474c4afc377db846ee187ddea4
SowjiKrish <120230840+SowjiKrish@users.noreply.github.com>
2022-12-21 21:18:55 +0530

    Add files via upload

---
+++ bill.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+printf("* * * * *BILL* * * * * *\n");
+printf("Item Quantity Price Amount\n");
+printf("Pen  5     4.00     20.000");
+return 0;
+
+
+}
---
+++ floatingshift.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main()
+{
+float a,b;
+printf("enter the floating point number");
+scanf ("%f",&a);
+b=a%10;
+printf("%f",b);
+return 0;
+
+}
