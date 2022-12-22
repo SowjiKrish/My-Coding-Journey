fb0caf10d1be57f4a9dc3b6646e926a0630abec5
SowjiKrish <120230840+SowjiKrish@users.noreply.github.com>
2022-12-22 21:51:47 +0530

    Add files via upload

---
+++ conditional.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("enter the values of a\n");
+  scanf("%d",&a);
+  printf("enter the value of b\n");
+  scanf("%d",&b);
+  c=(a>b)?a:b;
+  printf("%d",c);
+  return 0;
+
+
+
+}
