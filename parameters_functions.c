423cd07b9530c65c11a7d995795c7b7c0824f785
SowjiKrish <120230840+SowjiKrish@users.noreply.github.com>
2023-02-04 18:51:26 +0530

    Add files via upload

---
+++ parameters_functions.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void siblings(char name[],int age)
+{
+  printf("my name is %s,i am %d years old\n",name,age);
+}
+main()
+{
+siblings("santhi",24);
+siblings("prathap",21);
+siblings("sowji",19);
+}
